322accfac2924c4a36b07e3088d6c4db1aac87b7
theo753951 <48738166+theo753951@users.noreply.github.com>
2019-06-17 21:51:28 -0300

    Add files via upload

---
+++ src/Questão1e2/Animal.java
@@ -0,0 +1,19 @@
+package Questão1e2;
+
+public abstract class Animal {
+    private String nome;
+    
+    public Animal(String n){
+        setNome(n);
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String n){
+        this.nome=n;
+    }
+    public void imprime(){
+        System.out.println(this.getNome());
+    }
+    public abstract void talk();
+}
---
+++ src/Questão1e2/Mamifero.java
@@ -0,0 +1,19 @@
+package Questão1e2;
+
+public abstract class Mamifero extends Animal{
+    
+    private int patas;
+    
+    public Mamifero(String n,int p) {
+        super(n);
+        setPatas(p);
+    }
+    public int getPatas(){
+        return this.patas;
+    }
+    public void setPatas(int p){
+        this.patas=p;
+    }
+    public abstract void talk();
+    
+}
